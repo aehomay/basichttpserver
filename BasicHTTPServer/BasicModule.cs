b67456107770fdf0858a52089f832b6d6b4406eb
aehomay <a.e.homay@gmail.com>
2018-11-05 19:33:48 +0100

    The first clean commit

---
+++ BasicHTTPServer/BasicModule.cs
@@ -0,0 +1,33 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicHTTPServer
+{
+    public class BasicModule : NancyModule
+    {
+        decimal value = 0;
+        public BasicModule()
+        {
+            Get["/temperature/{value:decimal}", runAsync:true] = async (parameters, token) =>
+            {
+                return await Task.Run(() => 
+                {
+                    return "Value " + parameters.value + "25.5000";
+                });
+            };
+
+            Post["/temperature/{value:decimal}", runAsync:true] = async (parameters, token) => 
+            {
+                return await Task.Run(() => 
+                {
+                    value = parameters.value;
+                    return HttpStatusCode.OK;
+                });
+            };
+        }
+    }
+}
---
+++ BasicHTTPServer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicHTTPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://+:8080";
+
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine($"Nancy is Running on {url}");
+                Console.WriteLine("Press enter to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
